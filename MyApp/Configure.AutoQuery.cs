e5500892db3993669cb81e5c772a5119f2ddace3
Demis Bellot <demis.bellot@gmail.com>
2024-03-13 16:37:58 +0800

    Initial commit

---
+++ MyApp/Configure.AutoQuery.cs
@@ -0,0 +1,29 @@
+﻿using ServiceStack.Data;
+
+[assembly: HostingStartup(typeof(MyApp.ConfigureAutoQuery))]
+
+namespace MyApp;
+
+public class ConfigureAutoQuery : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Enable Audit History
+            services.AddSingleton<ICrudEvents>(c =>
+                new OrmLiteCrudEvents(c.GetRequiredService<IDbConnectionFactory>()));
+        })
+        .ConfigureAppHost(appHost => {
+
+            // For TodosService
+            appHost.Plugins.Add(new AutoQueryDataFeature());
+
+            // For Bookings https://docs.servicestack.net/autoquery-crud-bookings
+            appHost.Plugins.Add(new AutoQueryFeature
+            {
+                MaxLimit = 1000,
+                //IncludeTotal = true,
+            });
+
+            appHost.Resolve<ICrudEvents>().InitSchema();
+        });
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# blazor-vue
+
+.NET 8.0 Simple, Server Rendered Blazor Tailwind App with Vue interactivity
+
+[![](https://raw.githubusercontent.com/ServiceStack/Assets/master/csharp-templates/blazor-vue.png)](http://blazor-vue.web-templates.io/)
+
+> Browse [source code](https://github.com/NetCoreTemplates/blazor-vue), view live demo [blazor-vue.web-templates.io](http://blazor-vue.web-templates.io) and install with [dotnet-new](https://docs.servicestack.net/dotnet-new):
+
+    $ dotnet tool install -g x
+
+    $ x new blazor-vue ProjectName
+
+Alternatively write new project files directly into an empty repository, using the Directory Name as the ProjectName:
+
+    $ git clone https://github.com/<User>/<ProjectName>.git
+    $ cd <ProjectName>
+    $ x new blazor-vue
+
