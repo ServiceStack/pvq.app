e38dd1a418d8baef2434125753d61be515605a9c
Demis Bellot <demis.bellot@gmail.com>
2024-04-09 02:33:47 +0800

    improve auto sign in

--- MyApp.ServiceInterface/Data/StatUtils.cs
+++ MyApp.ServiceInterface/Data/StatUtils.cs
@@ -11,6 +11,31 @@ public static class StatUtils
     public static string? GetUserName(this ClaimsPrincipal? user) => user?.Identity?.Name;
     public static bool IsAdminOrModerator(this ClaimsPrincipal? user) => Stats.IsAdminOrModerator(user?.Identity?.Name);
     
+    public static AuthenticateResponse? ToAuthenticateResponse(this ClaimsPrincipal? user, Action<AuthenticateResponse>? configure=null)
+    {
+        if (user?.Identity is not { IsAuthenticated: true })
+            return null;
+        
+        var sub = user.FindFirst(JwtClaimTypes.Subject)?.Value;
+        var id = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var to = new AuthenticateResponse
+        {
+            UserId = sub ?? id, // sub can override Id
+            UserName = user.GetUserName(),
+            DisplayName = user.GetDisplayName(),
+            ProfileUrl = user.GetPicture(),
+            Roles = [..user.GetRoles()],
+        };
+        if (sub != null && id != null)
+        {
+            to.Meta = new() {
+                ["Id"] = user.GetUserId(),
+            };
+        }
+        configure?.Invoke(to);
+        return to;
+    }
+
     public static string? GetRemoteIp(this HttpContext? ctx)
     {
         var headers = ctx?.Request.Headers;
