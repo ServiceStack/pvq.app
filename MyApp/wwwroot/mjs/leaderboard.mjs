85a3f14edb8999c169f0e05f30f568b3d549dfda
Darren Reid <darren@reidmail.org>
2024-04-10 17:02:37 +1000

    leaderboard stats fixes. style fixes first pass at barchart

--- MyApp/wwwroot/mjs/leaderboard.mjs
+++ MyApp/wwwroot/mjs/leaderboard.mjs
@@ -5,26 +5,56 @@ import { mount, alreadyMounted, forceMount } from "app.mjs"
 import {GetMeta} from "./dtos.mjs";
 import Chart from "../posts/components/ChartJs.mjs";
 
-export default  {
+const modelColors = {
+    "gemma": "#1d4ed8",
+    "gemma-2b": "#60a5fa",
+    "mistral": "#b91c1c",
+    "gpt-4-turbo": "#047857",
+    "claude-3-opus": "#b45309",
+    "claude-3-haiku": "#44403c",
+    "claude-3-sonnet": "#78716c",
+}
+
+const modelAliases = {
+    "gemma": "Gemini",
+    "gemma-2b": "Gemini 2B",
+    "mistral": "Mistral",
+    "gpt-4-turbo": "GPT-4 Turbo",
+    "claude-3-opus": "Claude 3 Opus",
+    "claude-3-haiku": "Claude 3 Haiku",
+    "claude-3-sonnet": "Claude 3 Sonnet",
+}
+export default {
     async load() {
         const el = $1("#winrate-chart")
         if (!el) return
-        
+
         // Get data prop values from data attributes
-        const labelsArray = el.getAttribute("data-labels")
         const datasetsArray = el.getAttribute("data-datasets")
         const optionsObj = el.getAttribute("data-options")
-        
+
         // Get type prop value from data attribute
         const type = "bar"
-        
-        console.log({labelsArray, datasetsArray, optionsObj, type})
-        
+
         // Parse data attributes
-        const labels = JSON.parse(labelsArray)
         const datasets = JSON.parse(datasetsArray)
+        const modelNames = datasets[0].modelNames
+
+        // Create labels and data arrays based on the original order of modelNames
+        const labels = modelNames.map(modelName => modelAliases[modelName] || modelName);
+        const data = modelNames.map(modelName => datasets[0].data[modelNames.indexOf(modelName)]);
+
+        // Create the dataset object
+        const newDataset = {
+            label: "Win Rate %",
+            data: data,
+            backgroundColor: modelNames.map(modelName => modelColors[modelName] || "#000"),
+            borderColor: modelNames.map(modelName => modelColors[modelName] || "#000"),
+            borderWidth: 1
+        };
+
         const options = JSON.parse(optionsObj)
-        
-        mount(el, Chart, { type: type, data: {labels: labels, datasets: datasets}, options: options })
+
+        mount(el, Chart, { type: type, data: { labels: labels, datasets: [newDataset] }})
     }
 }
