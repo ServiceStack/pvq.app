191e4e0c1413c712b8fba3cb30a44c3c4d271c82
GitHub Action <action@github.com>
2024-04-10 11:42:42 +0000

    deploy: d684f499bc7f7b26d3d6911a8526168fde1ba084

---
+++ pages/Leaderboard/WinRates.mjs
@@ -0,0 +1,29 @@
+﻿import ChartJs, { colors } from "../components/ChartJs.mjs"
+
+export default {
+    components: { ChartJs },
+    template: `
+        <ChartJs :data="data" />
+    `,
+    props:['results'],
+    setup(props) {
+        let results = props.results
+        results.sort((a,b) => b.value - a.value)
+        results = results.slice(0,10)
+
+        const datasets = results.map((x,i) => ({
+            label: x.displayName,
+            backgroundColor: colors[i % colors.length].background,
+            borderColor: colors[i % colors.length].border,
+            borderWidth: 1,
+            data: [x.value]
+        }))
+
+        const data = {
+            labels: ['Win Rates'],
+            datasets
+        }
+
+        return { data }
+    }
+}
---
+++ pages/components/ChartJs.mjs
@@ -0,0 +1,42 @@
+﻿import { ref, onMounted } from "vue"
+import { addScript } from "@servicestack/client"
+
+const loadJs = addScript('/lib/js/chart.umd.min.js')
+
+export const colors = [
+    { background: 'rgba(201, 203, 207, 0.2)', border: 'rgb(201, 203, 207)' },
+    { background: 'rgba(255, 99, 132, 0.2)',  border: 'rgb(255, 99, 132)' },
+    { background: 'rgba(153, 102, 255, 0.2)', border: 'rgb(153, 102, 255)' },
+    { background: 'rgba(54, 162, 235, 0.2)',  border: 'rgb(54, 162, 235)' },
+    { background: 'rgba(255, 159, 64, 0.2)',  border: 'rgb(255, 159, 64)' },
+    { background: 'rgba(67, 56, 202, 0.2)',   border: 'rgb(67, 56, 202)' },
+    { background: 'rgba(255, 99, 132, 0.2)',  border: 'rgb(255, 99, 132)' },
+    { background: 'rgba(14, 116, 144, 0.2)',  border: 'rgb(14, 116, 144)' },
+    { background: 'rgba(162, 28, 175, 0.2)',  border: 'rgb(162, 28, 175)' },
+]
+
+
+export default {
+    template:`<div><canvas ref="chart"></canvas></div>`,
+    props:['type','data','options'],
+    setup(props) {
+        const chart = ref()
+        onMounted(async () => {
+            await loadJs
+
+            const options = props.options || {
+                responsive: true,
+                legend: {
+                    position: "top"
+                }
+            }
+            new Chart(chart.value, {
+                type: props.type || "bar",
+                data: props.data,
+                options,
+            })
+
+        })
+        return { chart }
+    }
+}
