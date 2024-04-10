7f8a594ac995f02639fffc9045f336eb3aa77d4e
GitHub Action <action@github.com>
2024-04-10 08:52:28 +0000

    deploy: 8bbee87110f8c25d171daa6ab65190f5b8e74f35

--- mjs/leaderboard.mjs
+++ mjs/leaderboard.mjs
@@ -13,6 +13,11 @@ const modelColors = {
     "claude-3-opus": "#b45309",
     "claude-3-haiku": "#44403c",
     "claude-3-sonnet": "#78716c",
+    "codellama": "#0ea5e9",
+    "deepseek-coder-6.7b": "#4f46e5",
+    "gemini-pro": "#8b5cf6",
+    "phi": "#c026d3",
+    "qwen-4b": "#581c87"
 }
 
 const modelAliases = {
@@ -23,38 +28,59 @@ const modelAliases = {
     "claude-3-opus": "Claude 3 Opus",
     "claude-3-haiku": "Claude 3 Haiku",
     "claude-3-sonnet": "Claude 3 Sonnet",
+    "codellama": "CodeLlama",
+    "deepseek-coder-6.7b": "DeepSeek Coder 6.7b",
+    "gemini-pro": "Gemini Pro",
+    "phi": "Phi",
+    "qwen-4b": "Qwen 1.5 4B"
 }
 export default {
     async load() {
-        const el = $1("#winrate-chart")
+        const el = $1("chart-js")
         if (!el) return
+        
+        // could be multiple charts on the page
+        $$("chart-js").forEach(el => {
+            // Get data prop values from data attributes
+            const datasetsArray = el.getAttribute("data-datasets")
+            const optionsObj = el.getAttribute("data-options")
 
-        // Get data prop values from data attributes
-        const datasetsArray = el.getAttribute("data-datasets")
-        const optionsObj = el.getAttribute("data-options")
+            // Get type prop value from data attribute
+            const type = "bar"
 
-        // Get type prop value from data attribute
-        const type = "bar"
+            console.log(datasetsArray);
+            // Parse data attributes
+            const datasets = JSON.parse(datasetsArray)
+            const modelNames = datasets[0].modelNames
 
-        // Parse data attributes
-        const datasets = JSON.parse(datasetsArray)
-        const modelNames = datasets[0].modelNames
+            // Create an object to map model names to their data and color
+            const modelData = {};
+            modelNames.forEach((modelName, index) => {
+                modelData[modelName] = {
+                    data: {
+                        x: modelName,
+                        y: datasets[0].data[index]
+                    }
+                };
+            });
 
-        // Create labels and data arrays based on the original order of modelNames
-        const labels = modelNames.map(modelName => modelAliases[modelName] || modelName);
-        const data = modelNames.map(modelName => datasets[0].data[modelNames.indexOf(modelName)]);
+            // Create labels and datasets based on the original order of modelNames
+            const labels = [];
+            const newDatasets = [];
 
-        // Create the dataset object
-        const newDataset = {
-            label: "Win Rate %",
-            data: data,
-            backgroundColor: modelNames.map(modelName => modelColors[modelName] || "#000"),
-            borderColor: modelNames.map(modelName => modelColors[modelName] || "#000"),
-            borderWidth: 1
-        };
+            modelNames.forEach(modelName => {
+                labels.push(modelName);
+            });
+            
+            newDatasets.push(
+                {
+                    data: modelNames.map(modelName => modelData[modelName].data),
+                    label: datasets[0].label,
+                    backgroundColor: modelNames.map(modelName => modelColors[modelName]),
+                }
+            )
 
-        const options = JSON.parse(optionsObj)
-
-        mount(el, Chart, { type: type, data: { labels: labels, datasets: [newDataset] }})
+            mount(el, Chart, { type: type, data: { datasets: newDatasets }})
+        })
     }
 }
