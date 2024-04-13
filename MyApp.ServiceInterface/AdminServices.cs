95696c9cc5985e6acd6a3abf7f567c05c24895a9
Demis Bellot <demis.bellot@gmail.com>
2024-04-13 16:44:24 +0800

    Add Sync Admin Tasks

---
+++ MyApp.ServiceInterface/AdminServices.cs
@@ -0,0 +1,32 @@
+﻿using MyApp.Data;
+using MyApp.ServiceModel;
+using ServiceStack;
+
+namespace MyApp.ServiceInterface;
+
+public class AdminServices(AppConfig appConfig) : Service
+{
+    public async Task<object> Any(Sync request)
+    {
+        if (request.Tasks != null)
+        {
+            var db = Db;
+            var tasks = request.Tasks;
+            if (tasks.Contains(nameof(AppConfig.ResetInitialPostId)))
+                appConfig.ResetInitialPostId(db);
+            if (tasks.Contains(nameof(AppConfig.ResetUsersReputation)))
+                appConfig.ResetUsersReputation(db);
+            if (tasks.Contains(nameof(AppConfig.ResetUsersQuestions)))
+                appConfig.ResetUsersQuestions(db);
+            if (tasks.Contains(nameof(AppConfig.ResetUsersUnreadAchievements)))
+                appConfig.ResetUsersUnreadAchievements(db);
+            if (tasks.Contains(nameof(AppConfig.ResetUsersUnreadNotifications)))
+                appConfig.ResetUsersUnreadNotifications(db);
+            if (tasks.Contains(nameof(AppConfig.UpdateUsersReputation)))
+                appConfig.UpdateUsersReputation(db);
+            if (tasks.Contains(nameof(AppConfig.UpdateUsersQuestions)))
+                appConfig.UpdateUsersQuestions(db);
+        }
+        return new StringResponse { Result = "OK" };
+    }
+}
---
+++ MyApp.ServiceModel/Moderation.cs
@@ -0,0 +1,9 @@
+﻿using ServiceStack;
+
+namespace MyApp.ServiceModel;
+
+[ValidateHasRole(Roles.Moderator)]
+public class Sync : IGet, IReturn<StringResponse>
+{
+    public List<string>? Tasks { get; set; }
+}
