07a45df942588abdd681dc3c1a7a2b68c025cc92
Demis Bellot <demis.bellot@gmail.com>
2024-04-23 11:58:25 +0800

    implement search all vs posts

--- MyApp.ServiceInterface/ApiServices.cs
+++ MyApp.ServiceInterface/ApiServices.cs
@@ -11,35 +11,58 @@ public class ApiServices(IDbConnectionFactory DbFactory) : Service
     public async Task<object> Any(SearchPosts request)
     {
         using var dbSearch = await DbFactory.OpenAsync(Databases.Search);
-        var q = dbSearch.From<PostFts>();
-            
-        var search = request.Q?.Trim() ?? "";
-        if (search.StartsWith('[') && search.EndsWith(']'))
-        {
-            q.WhereContainsTag(search.TrimStart('[').TrimEnd(']'));
-        }
-        else if (!string.IsNullOrEmpty(search))
-        {
-            var searchPhrase = string.Join(" AND ", search.Split(' ').Select(x => '"' + x.Trim().StripQuotes() + '"'));
-            q.Where("Body match {0}", searchPhrase);
-        }
 
-        q.OrderByView(request.View);
-
+        var skip = request.Skip;
         var take = Math.Min(request.Take ?? 25, 200);
-        List<PostFts> postsFts = await dbSearch.SelectAsync(q
-            .Select("RefId, substring(Body,0,400) as Body, ModifiedDate")
-            .Skip(request.Skip)
-            .Take(take));
-        var total = dbSearch.Count(q);
 
-        using var db = await DbFactory.OpenAsync();
-        var posts = await db.PopulatePostsAsync(postsFts);
-
-        return new SearchPostsResponse
+        var search = request.Q?.Trim() ?? "";
+        if (!string.IsNullOrEmpty(search))
         {
-            Total = total,
-            Results = posts,
-        };
+            var q = dbSearch.From<PostFts>();
+            if (search.StartsWith('[') && search.EndsWith(']'))
+            {
+                q.WhereContainsTag(search.TrimStart('[').TrimEnd(']'));
+            }
+            else
+            {
+                var searchPhrase = string.Join(" AND ", search.Split(' ').Select(x => '"' + x.Trim().StripQuotes() + '"'));
+                q.Where("Body match {0}", searchPhrase);
+            }
+
+            q.OrderByView(request.View);
+
+            List<PostFts> postsFts = await dbSearch.SelectAsync(q
+                .Select("RefId, substring(Body,0,400) as Body, ModifiedDate")
+                .Skip(request.Skip)
+                .Take(take));
+            var total = dbSearch.Count(q);
+
+            using var db = await DbFactory.OpenAsync();
+            var posts = await db.PopulatePostsAsync(postsFts);
+
+            return new SearchPostsResponse
+            {
+                Total = total,
+                Results = posts,
+            };
+        }
+        else
+        {
+            using var db = await DbFactory.OpenAsync();
+            var q = db.From<Post>();
+            
+            var posts = await db.SelectAsync(q
+                .OrderByView(request.View)
+                .Skip(skip)
+                .Take(take));
+
+            var total = db.Count(q);
+
+            return new SearchPostsResponse
+            {
+                Total = total,
+                Results = posts,
+            };
+        }
     }
 }
