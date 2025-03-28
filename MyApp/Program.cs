eded2ba49c9b501e7696f6abe7f126d27320278f
Demis Bellot <demis.bellot@gmail.com>
2025-03-28 16:51:31 +0800

    updates

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -82,13 +82,7 @@ else
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    // kamal deploy fails with: Failed to determine the https port for redirect.
-    app.UseHttpsRedirection();
 }
-
-// app.UseForwardedHeaders(new ForwardedHeadersOptions {
-//     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-// });  
 
 //TODO: Remove after bing search no longer includes these links
 string[] redirectPosts = [
